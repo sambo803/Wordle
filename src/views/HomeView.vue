21c3b5dc57c7784e23555c2057c4bb6312509fc4
Dan Cross <dancross96@gmail.com>
2022-09-12 14:04:26 -0500

    webpage loads

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,16 +18,16 @@ export default {
       this.word = this.word.split('')
     },
     checkWord: function (guess) {
-      console.log("checking word")
-      guess = guess.split('')
-      guess.forEach(function (letter, index) => {
-        console.log(this.word)
-        console.log(letter)
-        index++
-      })
-    console.log(this.evaluation)
-  }
-},
+      // console.log("checking word")
+      // guess = guess.split('')
+      // guess.forEach(function (letter, index) => {
+      //   console.log(this.word)
+      //   console.log(letter)
+      //   index++
+      // })
+      // console.log(this.evaluation)
+    }
+  },
 };
 </script>
 
